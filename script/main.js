1d80a829b0dc36e696237fadad58bfacd3605c95
starkoi <sonya.b@hotmail.ca>
2017-06-05 07:58:28 -0400

    modif pour expand search

--- script/main.js
+++ script/main.js
@@ -4,11 +4,11 @@ jQuery(function($){
     $( '.menu-btn' ).click(function(){
         $('.responsive-menu').toggleClass('expand')
     });
-    $( 'li.sub-menu').click(function(){
+    $( '.sub-menu').click(function(){
         $('.children').toggleClass('expand')
     });
     $( '.fa-search').click(function(){
-        $('#search').toggleClass('expand')
+        $('#barre_de_recherche').toggleClass('expand');
     })
 
 });
