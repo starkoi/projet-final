658a419be8981fb0a0a878bd2d9e95fbfc32d749
cyiphum <carolina.yiphum@gmail.com>
2017-06-06 08:17:45 -0400

    Ajouter la pagination

--- recettes.php
+++ recettes.php
@@ -1,13 +1,13 @@
 <?php
+$page_name = 'recettes';
+$name_main = 'recettes';
 require_once('views/page_top.php');
-//$nom= (isset($_GET['nom']) && ($_GET['nom']==1 ||($_GET['nom']==2 || ($_GET['nom']==3 ))))? $_GET['nom']: null;
 $cat_id = (isset($_GET['cat_id'])? $_GET['cat_id']: null);
 $recettes = get_recettes_avec_cats($cat_id);
 $recettes_temp = reset($recettes);
 
 ?>
 
-<main>
     <h1><?= (isset($_GET['cat_id'])? ucfirst($recettes_temp['cat_nom']): null) ?></h1>
     <?php
     foreach ($recettes as $id => $r){
@@ -16,8 +16,6 @@ $recettes_temp = reset($recettes);
         echo    '<div>'.
             $img.
             '<h2>' .ucfirst($r["nom"]) . '</h2>' .
-            '<p>' . "Temps de préparation ". $r["temps_preparation"] ." minutes ". "temps de cuisson ". $r["cuisson"]. " minutes". " portions ". $r["portion"] . " portions" .'</p>' .
-
             "<a href='?op=ajouter&itemid=".$id.
             ((!is_null($cat_id))?"&cat_id=".$cat_id : "").
             "' class='ajouter_panier'>Ajouter</a>".
@@ -25,8 +23,33 @@ $recettes_temp = reset($recettes);
             $r['id'].((!is_null($cat_id))?"&cat_id=".$cat_id : ""). '">Détails</a>'.
             '</div>';
     }
+    //quantité de recettes par page
+
+    $per_page= 2;
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+    } else{
+        $page = 1;
+    }
+
+    //la page commence en 0 et elle est multipliée $per_page
+    $start = ($page=1) * $per_page;
+    $query = 'SELECT * FROM recettes LIMIT $start, $per_page';
+    $query = 'SELECT * FROM recettes';
+    $result = mysqli_query($mysqli, $query);
+    $total_record = mysqli_num_rows($result);
+    $total_pages = ceil($total_record / $per_page);
+    //link pour la premiere page
+    echo "<a href = 'recettes.php?cat_id=$cat_id  page=1'>" . 'Première ' . "</a>";
+
+    for($i=1; $i<=$total_pages;$i++){
+        echo "<a href='recettes.php?cat_id=$cat_id page=". $i ."'>" . $i . "</a>";
+    }
+    //link pour la dernière page
+    echo "<a href='recettes.php?cat_id=$cat_id page=$total_pages'>" . ' Dernière'. "</a>";
     ?>
-</main>
+
+
 <?php require_once('views/page_bottom.php') ?>
 
 
