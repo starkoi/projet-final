68fc96035746fbfdd6f0d1eae94b61ebd4acfe3b
Carolinayi <carolina.yiphum@gmail.com>
2017-06-10 02:02:10 -0400

    Page favoris fini

--- favoris.php
+++ favoris.php
@@ -11,7 +11,7 @@ require_once('views/page_top.php');
 
 
     <form method="post" id="enveloppe-favori">
-        <section id="section-favori" class=" col-m-12 col-9">
+        <section id="section-favori" class=" col-m-9 col-9">
 
 
             <?php foreach ($favori as $recette_id => $cat_id) {
@@ -21,19 +21,20 @@ require_once('views/page_top.php');
                 $item_name = 'cart_item_' . $recette_id;
                 ?>
 
-                <?php  echo '<div class="container-favori col-m-6 col-4">';?>
+                <?php  echo '<div class="container-favori col-m-4 col-4">';?>
 
                 <?php  echo"<img class=\"favori-image col-m-12 col-12\" src=\"$img_recette\" alt=\"\"/>";?>
                 <?php  echo"<label for=\"$item_name\"></label>";?>
                 <?php  echo"<h3 id='title-favori'>" . ucfirst($detail_recette[$recette_id]['nom'])."</h3>";?>
+                <a href='?op=maz&itemid=<?= $recette_id ?>' class="btn_retirer">X</a>
                 <?php  echo'<a class="en-savoir-plus-favori" href="details.php?produit_id=' .
                     $recette_id.((!is_null($cat_id))?"&cat_id=".$cat_id : "").'">Voir la recette</a>'; ?>
-                <a href='?op=maz&itemid=<?= $recette_id ?>' class="btn_retirer">X</a>
                 <?php echo "</div>"?>
 
             <?php } ?>
         </section>
-        <aside id="aside-favori" class="col-m-12 col-2">
+        <aside id="aside-favori" class="col-m-2 col-2">
+            <img class="pub-favori col-m-3 col-12" src="images/publicite/publicite-eskal.jpg" alt="publicité d\'eskal"/>
             <img class="pub-favori col-m-3 col-12" src="images/publicite/publicite-mccormick.jpg" alt="publicité de mccormick"/>
             <img class="pub-favori col-m-3 col-12" src="images/publicite/publicite-island-oasis.jpg" alt="publicité d\island-oasis"/>
         </aside>
@@ -42,7 +43,7 @@ require_once('views/page_top.php');
             echo   '<div id="vider-favori"><input id="button-vider-favori" type="button" value="Vider les favoris" onclick="document.location=\'?op=vider\'"></div>';
 
         } else {
-            echo "<h2>Vous n'avez pas des favoris</h2>";
+            echo "<h2 id='favori-vide'>Vous n'avez pas de favoris</h2>";
         }
         ?>
 
