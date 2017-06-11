556d726387bcfc769a87ad2cbba8dd5aa81566a0
starkoi <sonya.b@hotmail.ca>
2017-06-10 22:50:43 -0400

    ajout alt

--- views/footer.php
+++ views/footer.php
@@ -10,10 +10,10 @@
         </ul>
     </div>
     <ul id="reseaux-sociaux">
-        <li><a href="https://www.instagram.com"><img src="images/social/instagram.png"></a></li>
-        <li><a href="http://www.facebook.com"><img src="images/social/facebook.png"></a></li>
-        <li><a href="http://www.youtube.com"><img src="images/social/youtube.png"></a></li>
-        <li><a href="http://www.twitter.com"><img src="images/social/twitter.png"></a></li>
+        <li><a href="https://www.instagram.com"><img src="images/social/instagram.png" alt="logo d'instagram"></a></li>
+        <li><a href="http://www.facebook.com"><img src="images/social/facebook.png" alt="logo de facebook"></a></li>
+        <li><a href="http://www.youtube.com"><img src="images/social/youtube.png" alt="logo de youtube"></a></li>
+        <li><a href="http://www.twitter.com"><img src="images/social/twitter.png" alt="logo de twitter"></a></li>
     </ul>
     <a href="#"><img  id="top" src="images/top.png" alt="image fleche vers le haut"/></a>
 </footer>
